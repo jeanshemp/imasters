6c821e2b845f9050b672c64280f98a345bafff76
Jean Carlos <sheemps@gmail.com>
2014-04-11 15:09:35 -0300

    Exercicio 4 forum imasters
    http://forum.imasters.com.br/topic/465210-exercicios-para-iniciantes/

---
+++ Exercicio4.java
@@ -0,0 +1,53 @@
+/*
+ * 4 - Escreva um programa para ler 3 notas diferentes de um aluno e informar
+o valor da sua maior nota.
+
+Altere este programa para informar também se a maior nota foi a 
+primeira, a segunda ou a terceira
+ */
+package javaaulass;
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		Scanner Dados = new Scanner (System.in);
+		int notaAv1;
+		int notaAv2;
+        int notaAv3;
+        
+        
+        System.out.println ("Digite  a Nota da Avaliação 1 : ");
+        notaAv1 = Dados.nextInt();
+        System.out.println ("Digite a Nota da Avaliação 2 : ");
+        notaAv2 = Dados.nextInt();
+        System.out.println ("Digite  a Nota da Avaliação 3 :");
+        notaAv3 = Dados.nextInt();
+        
+        if (notaAv1 > notaAv2 && notaAv1 > notaAv3){
+        	System.out.println ("A Nota da Avaliação 1 foi a maior nota, Parabens, Nota = " + notaAv1);
+        }
+        
+        if (notaAv2 > notaAv1 && notaAv2 > notaAv3){
+        	System.out.println ("A Nota da Avaliação 2 foi a maior, Parabens! Nota = " + notaAv2);
+        }
+        
+        if (notaAv3 > notaAv2 && notaAv3 > notaAv1){
+        	System.out.println ("A Nota da Avaliação 3 foi a maior, Parabens! Nota = " + notaAv3);
+        }
+        
+        if (notaAv1 < notaAv2 && notaAv1 < notaAv3){
+        	System.out.println ("A Nota da Avaliação 1 foi a menor. Nota = " + notaAv1);
+        }
+        
+        if (notaAv2 < notaAv1 && notaAv2 < notaAv3){
+        	System.out.println ("A Nota da Avaliação 2 foi a menor. Nota = " + notaAv2);
+        }
+        
+        if (notaAv3 < notaAv2 && notaAv3 < notaAv1){
+        	System.out.println ("A Nota da Avaliação 3 foi a menor. Nota = " + notaAv3);
+        }
+        
+	}
+
+}
